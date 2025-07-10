3b160d248a83741d4d8f9b57ed166d619b18f508
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:04:00 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "School_Adventures" `
+  -ModName "School Adventures" `
+  -ModFolder "school_adventures" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/school-adventures/School-Adventures-Original.zip" `
+  -ModBaseFilesUrlHash "ba803de702c91b1a6bf0a2112d9f7cd66c83384e516bd5891f23fb6380978fa6" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
